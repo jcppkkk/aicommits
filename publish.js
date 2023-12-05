121f4f601706b8096ee7e5430fa350f5e2f4e628
Jethro Yu <jethro@teamt5.org>
2023-12-05 17:48:14 +0800

    feat(publish): Add publish.js script and update package.json

---
+++ publish.js
@@ -0,0 +1,19 @@
+import { execSync } from 'child_process';
+import path from 'path';
+
+function executeCommand(command) {
+    return execSync(command).toString().trim();
+}
+
+const remoteUrl = executeCommand('git config --get remote.origin.url');
+const branchName = executeCommand('git rev-parse --abbrev-ref HEAD');
+
+const urlParts = remoteUrl.split('/');
+const projectName = path.basename(urlParts[urlParts.length - 1], '.git');
+const userName = urlParts[urlParts.length - 2].split(':')[1];
+
+console.log(`Username: ${userName}, Project: ${projectName}, Branch: ${branchName}`);
+
+executeCommand('git push');
+executeCommand('pnpm dlx git-publish');
+executeCommand(`sudo npm -g i '${userName}/${projectName}#npm/${branchName}'`);
